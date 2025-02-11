2448241d6cce7b8d3ded090fd9166cd40ea94db2
Ricki Angel <angelricki@outlook.com>
2025-02-11 08:27:10 +0000

    Initiate work on a Load Instruction class.

---
+++ src/sml/instruction/loadInstruction.java
@@ -0,0 +1,22 @@
+package sml.instruction;
+
+import sml.Frame;
+import sml.Instruction;
+import sml.Machine;
+
+import java.util.Optional;
+
+public class loadInstruction extends Instruction {
+
+
+    public class LoadInstruc""
+    @Override
+    public Optional<Frame> execute(Machine machine) {
+        return Optional.empty();
+    }
+
+    @Override
+    protected String getOperandsString() {
+        return "";
+    }
+}
