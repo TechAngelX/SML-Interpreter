fbb9ad871148cf2ec8e7e9702d68129327431bcb
Ricki Angel <angelricki@outlook.com>
2025-02-14 07:48:13 +0000

    feat: Add AddInstruction initial implementation.

--- src/sml/instruction/AddInstruction.java
+++ src/sml/instruction/AddInstruction.java
@@ -1,4 +1,26 @@
 package sml.instruction;
 
-public class AddInstruction {
+import sml.Frame;
+import sml.Instruction;
+import sml.Label;
+import sml.Machine;
+
+import java.util.Optional;
+
+public class AddInstruction extends Instruction {
+    public static final String OP_CODE = "add";
+
+    public AddInstruction(Label label) {
+        super(label, OP_CODE);
+    }
+
+    @Override
+    public Optional<Frame> execute(Machine machine) {
+        return Optional.empty();
+    }
+
+    @Override
+    protected String getOperandsString() {
+        return "";
+    }
 }
