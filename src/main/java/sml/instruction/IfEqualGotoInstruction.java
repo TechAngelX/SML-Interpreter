bdc66ac170143b6967d1dc90dd30193b5bb8ab5d
Ricki Angel <angelricki@outlook.com>
2025-02-15 09:34:28 +0000

    feat: Add IfEqualGotoInstruction initial implementation

--- src/main/java/sml/instruction/IfEqualGotoInstruction.java
+++ src/main/java/sml/instruction/IfEqualGotoInstruction.java
@@ -1,4 +1,39 @@
 package sml.instruction;
 
-public class IfEqualGotoInstruction {
+import sml.*;
+import java.util.Optional;
+
+/**
+ * Equal comparison and Goto instruction for SML.
+ * Pops two values from stack and compares them.
+ * If values are equal, jumps to specified label;
+ * else, continue to next instruction.
+ */
+public class IfEqualGotoInstruction extends Instruction {
+    public static final String OP_CODE = "if_cmpeq";
+    private final Label jumpLabel;
+
+    public IfEqualGotoInstruction(Label label, Label jumpLabel) {
+        super(label, OP_CODE);
+        this.jumpLabel = jumpLabel;
+    }
+
+    @Override
+    public Optional<Frame> execute(Machine machine) {
+        Frame frame = machine.frame();
+        int value2 = frame.pop(); // Remember LIFO pop order.
+        int value1 = frame.pop();
+
+        if (value1 == value2) {   // If values are equal, jump to specified label.
+            return Optional.of(frame.jumpTo(jumpLabel));
+        } else {
+            // Otherwise, advance to next instruction.
+            return Optional.of(frame.advance());
+        }
+    }
+
+    @Override
+    protected String getOperandsString() {
+        return jumpLabel.toString();
+    }
 }
