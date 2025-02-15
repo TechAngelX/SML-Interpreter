fcda14cad6a0f3d4e3415e77cebd3354910923e6
Ricki Angel <angelricki@outlook.com>
2025-02-15 00:20:51 +0000

    refactor: rename CmpGotoInstruction to IfGreaterGotoInstruction.

--- src/main/java/sml/instruction/CmpGotoInstruction.java
+++
@@ -1,4 +0,0 @@
-package sml.instruction;
-
-public class CmpGotoInstruction {
-}
---
+++ src/main/java/sml/instruction/IfGreaterGotoInstruction.java
@@ -0,0 +1,44 @@
+package sml.instruction;
+
+import sml.Frame;
+import sml.Instruction;
+import sml.Label;
+import sml.Machine;
+
+import java.util.Optional;
+
+/**
+ * Compare amd Goto instruction for SML.
+ * Pops two values from the stack and compares them.
+ * If the first value is greater than the second, jumps to a
+ * specified label; else, continues to next instruction.
+ */
+public class IfGreaterGotoInstruction extends Instruction {
+    public static final String OP_CODE = "if_cmpgtL";
+    private final Label jumpLabel;
+
+    public IfGreaterGotoInstruction(Label label, Label jumpLabel) {
+        super(label, OP_CODE);
+        this.jumpLabel = jumpLabel;
+    }
+
+    @Override
+    public Optional<Frame> execute(Machine machine) {
+        Frame frame = machine.frame();
+        int value2 = frame.pop();
+        int value1 = frame.pop();
+
+        if (value1 > value2) {
+            // If value1 > value2, jump to specified label
+            return Optional.of(frame.jumpTo(jumpLabel));
+        } else {
+            // Otherwise, advance to next instruction
+            return Optional.of(frame.advance());
+        }
+    }
+
+    @Override
+    protected String getOperandsString() {
+        return jumpLabel.toString();
+    }
+}
