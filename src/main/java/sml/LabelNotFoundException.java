679f3fed2cdf7be18afa67aecfb859c9b681be96
Ricki Angel <angelricki@outlook.com>
2025-02-16 00:22:20 +0000

    refactor: Improve LabelNotFoundException with private label and method fields, add  getters for better exception handling

--- src/main/java/sml/LabelNotFoundException.java
+++ src/main/java/sml/LabelNotFoundException.java
@@ -1,7 +1,20 @@
 package sml;
 
 public class LabelNotFoundException extends RuntimeException {
+    private final Label label;
+    private final Method method;
+
     public LabelNotFoundException(Label label, Method method) {
         super("Label " + label + " not found in " + method.name());
+        this.label = label;
+        this.method = method;
+    }
+
+    public Label getLabel() {
+        return label;
+    }
+
+    public Method getMethod() {
+        return method;
     }
 }
