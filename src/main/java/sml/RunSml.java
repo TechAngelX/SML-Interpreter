fd80c05c071eb27f8ecf75651fa22204a63e1c7f
Ricki Angel <angelricki@outlook.com>
2025-02-22 22:12:17 +0000

    feat: Add Constructor-based Manual Dependency Injection.

--- src/main/java/sml/RunSml.java
+++ src/main/java/sml/RunSml.java
@@ -5,27 +5,46 @@ import java.util.Collection;
 
 public class RunSml {
     /**
-     * ================================================================
+     * =========================================================================================================
      * Entry point for executing Simple Machine Language (SML) programs.
-     * ================================================================
+     * =========================================================================================================
      *
-     * Provides the main method to:
-     * - Load SML programs from a file
-     * - Translate program instructions
-     * - Execute the program using the Machine interpreter
-     *
-     * Handles program loading, translation, and execution with basic
+     * Provides the main method to load SML programs from a file, translate program instructions and Execute the
+     * program using the Machine interpreter. Handles program loading, translation, and execution with basic
      * error handling for file reading and argument validation.
      *
+     * Dependency Injection:
+     * - Constructor-based manual DI
+     * - Explicit component control
+     *
      * Command-line Usage:
+     * Compile sml folder to /target/classes directory.
+     * Navigate to root folder
      * Run the program with: java sml.RunSml src/main/resources/test1.sml
+     *
      * @author Ricki Angel
      */
+
+    private final Translator translator;
+    private final Machine machine;
+
+    public RunSml(Translator translator, Machine machine) {
+        this.translator = translator;
+        this.machine = machine;
+    }
+
+    public void run(String filename) throws IOException {
+        Collection<Method> instructions = translator.readAndTranslate(filename);
+        machine.setProgram(instructions);
+        machine.execute();
+    }
+
     public static void main(String... args) {
         if (args.length != 1) {
             System.err.println("Incorrect number of arguments - sml.RunSml <file> - required");
             System.exit(-1);
         }
+
 
         try {
             Translator t = new Translator();
