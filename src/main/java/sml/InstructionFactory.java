4f16f6032d2fef5251cf31b0884f6028edb6dca5
Ricki Angel <angelricki@outlook.com>
2025-02-27 22:20:29 +0000

    fix: Discovery now excludes file called package-info.class

--- src/main/java/sml/InstructionFactory.java
+++ src/main/java/sml/InstructionFactory.java
@@ -158,8 +158,7 @@ public class InstructionFactory {
             if (packageDir.exists()) {
                 java.io.File[] classFiles = packageDir.listFiles(
                         file -> file.isFile() && file.getName().endsWith(".class")
-                        && !file.getName().equals("-package-info")
-
+                        && !file.getName().equals("package-info.class")
                 );
                 if (classFiles != null) {
                     for (java.io.File classFile : classFiles) {
